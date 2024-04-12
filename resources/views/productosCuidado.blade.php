6ef6738ba5db32cbd91238592568190e2063678d
Diego <diego.guevara@catolica.edu.sv>
2024-04-12 17:34:50 -0600

    Cambio en banner

--- resources/views/productosCuidado.blade.php
+++ resources/views/productosCuidado.blade.php
@@ -21,7 +21,13 @@
                 </div>
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                    <img src="./images/bannerCuida1.png" class="d-block w-100" alt="...">
+
+
+                        <!-- CAMBIAR POR OTRO BANNER POR FAVOR-->
+
+
+
+                    <img src="./images/bannerCuida2.png" class="d-block w-100" alt="...">
                     </div>
                     <div class="carousel-item">
                     <img src="./images/bannerCuida2.png" class="d-block w-100" alt="...">
