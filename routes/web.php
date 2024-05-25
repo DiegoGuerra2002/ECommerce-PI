ef690f2b1c8fad7ef120246f2f7f3b91ca9d539a
Diego <diego.guevara@catolica.edu.sv>
2024-05-24 23:46:57 -0600

    comentarios en rutas

--- routes/web.php
+++ routes/web.php
@@ -101,37 +101,37 @@ Route::group([], function(){
 Route::get('/productosSECRETOEDITAR', function () {
     return view('editar');
 })->name('editar');
-
+//Rutas de carrito de productos frescos
 Route::get('/carrito', [ProductoController::class, 'showCarrito'])->name('carrito.show');
 Route::get('carrito', [ProductoController::class, 'productoCarrito'])->name('carrito');
 Route::get('deleteCartItem/{id}', [ProductoController::class, 'deleteCartItem'])->name('deleteCartItem');
 Route::get('emptyCart', [ProductoController::class, 'emptyCart'])->name('emptyCart');
 Route::get('/producto/{id}', [ProductoController::class, 'agregaralcarrito'])->name('agregaralcarrito');
-
+//Rutas de carrito de productos congelados
 Route::get('/carrito', [PcoductoController::class, 'showCarrito'])->name('carrito.show');
 Route::get('carrito', [PcoductoController::class, 'productoCarrito'])->name('carrito');
 Route::get('deleteCartItem/{id}', [PcoductoController::class, 'deleteCartItem'])->name('deleteCartItem');
 Route::get('emptyCart', [PcoductoController::class, 'emptyCart'])->name('emptyCart');
 Route::get('/pcoducto/{id}', [PcoductoController::class, 'agregaralcarrito'])->name('agregaralcarritoC');
-
+//Rutas de carrito de productos de Cuidado Personal
 Route::get('/carrito', [PpoductoController::class, 'showCarrito'])->name('carrito.show');
 Route::get('carrito', [PpoductoController::class, 'productoCarrito'])->name('carrito');
 Route::get('deleteCartItem/{id}', [PpoductoController::class, 'deleteCartItem'])->name('deleteCartItem');
 Route::get('emptyCart', [PpoductoController::class, 'emptyCart'])->name('emptyCart');
 Route::get('/ppoducto/{id}', [PpoductoController::class, 'agregaralcarrito'])->name('agregaralcarritoCuidado');
-
+//Rutas de carrito de productos para el hogar
 Route::get('/carrito', [PhoductoController::class, 'showCarrito'])->name('carrito.show');
 Route::get('carrito', [PhoductoController::class, 'productoCarrito'])->name('carrito');
 Route::get('deleteCartItem/{id}', [PhoductoController::class, 'deleteCartItem'])->name('deleteCartItem');
 Route::get('emptyCart', [PhoductoController::class, 'emptyCart'])->name('emptyCart');
 Route::get('/phoducto/{id}', [PhoductoController::class, 'agregaralcarrito'])->name('agregaralcarritoH');
-
+//Rutas de carrito de productos para las mascotas
 Route::get('/carrito', [PmoductoController::class, 'showCarrito'])->name('carrito.show');
 Route::get('carrito', [PmoductoController::class, 'productoCarrito'])->name('carrito');
 Route::get('deleteCartItem/{id}', [PmoductoController::class, 'deleteCartItem'])->name('deleteCartItem');
 Route::get('emptyCart', [PmoductoController::class, 'emptyCart'])->name('emptyCart');
 Route::get('/pmoducto/{id}', [PmoductoController::class, 'agregaralcarrito'])->name('agregaralcarritoM');
-
+//Rutas de carrito de bebidas
 Route::get('/carrito', [PbebidaController::class, 'showCarrito'])->name('carrito.show');
 Route::get('carrito', [PbebidaController::class, 'productoCarrito'])->name('carrito');
 Route::get('deleteCartItem/{id}', [PbebidaController::class, 'deleteCartItem'])->name('deleteCartItem');
